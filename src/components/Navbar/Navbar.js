d6137a9e42c3231ccb172976ad6289a6be1bbaef
richa-dasila <128730998+richa-dasila@users.noreply.github.com>
2024-02-22 00:11:08 +0530

    improved links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,14 +18,14 @@ function Navbar() {
           <div className="collapse navbar-collapse flex-row-reverse " id="navbarTogglerDemo02">
             <ul className="navbar-nav ">
               <li className="nav-item me-3 ms-3">
-                <a className="nav-link " aria-current="page" target="_blank" rel=" noreferrer" href="https://github.com/FinalYearProject22/academic-report">
+                <a className="nav-link " aria-current="page" target="_blank" rel=" noreferrer" href="https://github.com/richa-dasila/report">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                 </svg>
                 &nbsp;Github</a>
               </li>
               <li className="nav-item me-3 ms-3">
-                <a className="nav-link " href="https://finalyearproject22.github.io/academic-report/" target="_blank" rel=" noreferrer">
+                <a className="nav-link " href="https://richa-dasila.github.io/report/" target="_blank" rel=" noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
                    <path fillRule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                     <path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
@@ -33,7 +33,7 @@ function Navbar() {
                 &nbsp;Project Home</a>
               </li>
               <li className="nav-item me-3 ms-3">
-                <a className="nav-link " href="ghfd" >
+                <a className="nav-link " href="https://richa-dasila.github.io/report/" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-person" viewBox="0 0 16 16">
                   <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                   <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5v2z"/>
