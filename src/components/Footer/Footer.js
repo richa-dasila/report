52d80aee1b73b84cd6fa416528150ba770ea9605
richa-dasila <128730998+richa-dasila@users.noreply.github.com>
2024-02-22 00:38:07 +0530

    improved links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
                         <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 
                         <h6 className="text-uppercase fw-bold mb-4">
-                        <a className="navbar-brand text-reset" target="_blank" rel=" noreferrer" href="https://finalyearproject22.github.io/academic-report/">
+                        <a className="navbar-brand text-reset" target="_blank" rel=" noreferrer" href="https://richa-dasila.github.io/report/">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-kanban" viewBox="0 0 16 16">
                             <path d="M13.5 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h11zm-11-1a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2h-11z"/>
                             <path d="M6.5 3a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm-4 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm8 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3z"/>
@@ -54,13 +54,13 @@ function Footer() {
                             Useful links
                         </h6>
                         <p>
-                        <a target="_blank" rel=" noreferrer"href="https://richa-dasila.github.io/report/" className="text-reset">Github</a>
+                        <a target="_blank" rel=" noreferrer"href="https://github.com/richa-dasila/report" className="text-reset">Github</a>
                         </p>
                         <p>
-                            <a href="#!" className="text-reset">Project Home</a>
+                            <a href="https://richa-dasila.github.io/report/" className="text-reset">Project Home</a>
                         </p>
                         <p>
-                            <a href="#!" className="text-reset">About</a>
+                            <a href="https://richa-dasila.github.io/report/" className="text-reset">About</a>
                         </p>
                         </div>
         
